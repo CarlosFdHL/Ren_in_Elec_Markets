93809244b89611e6fe0a0f20da5ebcd1e3e57b54
adamgoplo <adam.sociale@gmail.com>
2025-02-17 23:40:48 +0100

    added file sensitivity ''' ''' [main 3b3b3b3] added file sensitivity  5 files changed, 4 insertions(+), 4 deletions(-)  create mode 100644 step3_adam2/sensitivity.py ''' ''' Enumerating objects: 10, done. Counting objects: 100% (10/10), done. Delta compression using up to 8 threads Compressing objects: 100% (6/6), done. Writing objects: 100% (6/6), 1.02 KiB | 1.02 MiB/s, done. Total 6 (delta 4), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (4/4), completed with 4 local objects.

--- step3_adam2/main.py
+++ step3_adam2/main.py
@@ -1,9 +1,4 @@
-from input_data import *
-from model import Step1_model
-
-
-
-
+"""
 if __name__ == "__main__":
     # Solves the model with the data provided in the input_data.py file
 
@@ -12,4 +7,20 @@ if __name__ == "__main__":
     model.run()
     model.print_results()
 
+    print("End of main.py")
+"""
+
+from input_data import InputData
+from model import Step1_model
+
+def run_model(input_data):
+    # Solves the model with the data provided in the input_data.py file
+    model = Step1_model(input_data)
+    model.run()
+    model.print_results()
+
+if __name__ == "__main__":
+    # Default input data
+    input_data = InputData(generators, bid_offers, system_demand, demand_per_load, bus_reactance, bus_capacity)
+    run_model(input_data)
     print("End of main.py")
