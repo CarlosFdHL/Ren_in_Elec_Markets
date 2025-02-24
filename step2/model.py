9dd21d96a9bb7ca78954729f30cb68fed2760cd4
Kacper <rokosz@kacper.com>
2025-02-24 15:42:14 +0100

    adjustemnts to step2

--- step2/model.py
+++ step2/model.py
@@ -98,9 +98,9 @@ class Step2_model:
         }
 
         self.constraints.demand_equal_production = {
-            t: self.model.addConstr( - (gp.quicksum(self.variables.production[g, t] for g in self.data.generators) + 
-                                    (self.variables.battery_charging_power[t] * self.data.battery_change_efficiency) -  
-                                    self.variables.battery_discharging_power[t] * self.data.battery_discharge_efficiency),
+            t: self.model.addConstr( - (gp.quicksum(self.variables.production[g, t] for g in self.data.generators) - 
+                                    ((self.variables.battery_charging_power[t] * self.data.battery_change_efficiency) -  
+                                    self.variables.battery_discharging_power[t] / self.data.battery_discharge_efficiency)),
                                     GRB.EQUAL, 
                                     - (gp.quicksum(self.variables.demand[d, t] for d in self.data.loads)), 
                                     name=f"SystemDemandEqualProductionHour_{t}")
@@ -136,7 +136,7 @@ class Step2_model:
 
         #self.model.addConstr(self.variables.stored_energy[24], GRB.EQUAL, 0, name=f"StoredEnergy_24")
         
-        #self.model.addConstr(self.variables.stored_energy[1], GRB.EQUAL, 0, name=f"StoredEnergy_1")
+        self.model.addConstr(self.variables.stored_energy[1], GRB.EQUAL, 0, name=f"StoredEnergy_1")
         
         
     def build_objective_function(self):
--- step2/plotting.py
+++ step2/plotting.py
@@ -5,17 +5,21 @@ import numpy as np
 def plotting_results(model):
 # Plotting the results of the optimization problem
     t, price = zip(*model.results.price.items())
-    plt.step(t, price)
-    plt.xlabel("Hour")
-    plt.ylabel("Price $/MWh")
-    plt.title("Market clearing price for each hour")
-    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.plot(t, price)
+    ax.set_xlabel("Hour")
+    ax.set_ylabel("Price $/MWh")
+    ax.set_title("Market clearing price for each hour")
 
     _, stored_energy = zip(*model.variables.stored_energy.items())
+    fig, ax = plt.subplots()
     stored_energy = [float(value.X) for value in stored_energy]
-    plt.step(t, stored_energy)
-    plt.xlabel("Hour")
-    plt.ylabel("Energy stored MWh")
-    plt.title("Energy stored in battery for each hour")
-    plt.xticks(range(1, 25))
+    ax.step(t, stored_energy)
+    ax.set_xlabel("Hour")
+    ax.set_ylabel("Energy stored MWh")
+    ax.set_title("Energy stored in battery for each hour")
+    ax.set_xticks(range(1, 25))
+    
+    
     plt.show()
