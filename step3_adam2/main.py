aeff36a345aa092824a8082f9fbf86ce068b376d
adamgoplo <adam.sociale@gmail.com>
2025-02-25 21:52:58 +0100

    Sensitivity plot and eeded change in main

--- step3_adam2/main.py
+++ step3_adam2/main.py
@@ -17,11 +17,13 @@ def run_model(input_data):
     # Solves the model with the data provided in the input_data.py file
     model = Step1_model(input_data)
     model.run()
-    model.print_results()
+    model.print_results()  
 
+    return model.results.nodal_price  
 
 if __name__ == "__main__":
     # Default input data
     input_data = InputData(generators, bid_offers, system_demand, demand_per_load, bus_reactance, bus_capacity)
-    run_model(input_data)
+    nodal_prices = run_model(input_data)  
     print("End of main.py")
+    
