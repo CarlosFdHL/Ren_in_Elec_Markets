1b873838a933769cb9b6ccc7ef1e2e409cb9d2e6
adamgoplo <adam.sociale@gmail.com>
2025-03-19 14:12:09 +0100

    Working sensitivity for zonal prices

--- step3 Diego/main.py
+++ step3 Diego/main.py
@@ -2,7 +2,13 @@ from input_data import *
 from model import Step3_zonal
 
 
+def run_model(input_data):
+    # Solves the model with the data provided in the input_data.py file
+    model = Step3_zonal(input_data)
+    model.run()
+    model.print_results()  
 
+    return model.results.zonal_price  
 
 if __name__ == "__main__":
     # Solves the model with the data provided in the input_data.py file
