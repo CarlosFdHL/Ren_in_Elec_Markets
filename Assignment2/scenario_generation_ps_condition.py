f2c0523b1edf9412f54ce319d58e952aa110f61b
CarlosFdHL <148780937+CarlosFdHL@users.noreply.github.com>
2025-03-31 17:06:27 +0200

    system condition scenario generation

---
+++ Assignment2/scenario_generation_ps_condition.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+num_scenarios = 10
+num_hours = 24
+
+probability = 0.9
+
+# Generate data
+data = np.random.binomial(1, probability, (num_scenarios, num_hours))
+
+# Store in csv
+# df = pd.DataFrame(data, columns=[f'Hour_{i+1}' for i in range(num_hours)])
+# df.to_csv('ps_condition_scenarios.csv', index=False)
