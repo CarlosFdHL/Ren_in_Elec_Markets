f50743aae67bcb2c3661cf9348f3a70d2985b60f
CarlosFdHL <148780937+CarlosFdHL@users.noreply.github.com>
2025-05-16 18:17:48 +0200

    uncommented plots main2

--- Assignment2/first_task/main2.py
+++ Assignment2/first_task/main2.py
@@ -68,76 +68,76 @@ if __name__ == "__main__":
 
     # 4. Plot Profit Volatility vs. Beta
 
-    # plt.figure(figsize=(8, 6))
-    # plt.plot(beta_values, [res['total_expected_profit'] for res in results], '-')
-    # plt.xlabel('Risk Weight (Beta)')
-    # plt.ylabel('Total Expected Profit (€)')
-    # plt.grid(True)
-    # plt.tight_layout()
+    plt.figure(figsize=(8, 6))
+    plt.plot(beta_values, [res['total_expected_profit'] for res in results], '-')
+    plt.xlabel('Risk Weight (Beta)')
+    plt.ylabel('Total Expected Profit (€)')
+    plt.grid(True)
+    plt.tight_layout()
 
-    # # 5. Plot Profit per Scenario vs Beta
-    # fig, ax = plt.subplots(1,2,figsize=(12, 3))
-    # colors = plt.get_cmap('tab10').colors  # 10 colores bien diferenciados
-    # color_map = {0: colors[0], 0.8: colors[1], 1: colors[2]}
-    # j = -1
-    # for i, res in enumerate(results):
-    #     if res['beta'] == 0 or res['beta'] == 1 or res['beta'] == 0.5:
-    #         j += 1
-    #         ax[0].hist(res['profit_per_scenario'], label=f"$\\beta = {res['beta']:.2f}$",
-    #                 bins=30, alpha=0.3, color=colors[j], edgecolor='black')
-    # ax[0].legend()
-    # ax[0].grid()
-    # ax[0].set_xlabel('Profit per Scenario (€)')
-    # ax[0].set_ylabel('Frequency')
-    # ax[1].plot(beta_values, [res['total_expected_profit'] for res in results], '-')
-    # ax[1].set_xlabel('Risk Weight (Beta)')
-    # ax[1].set_ylabel('Total Expected Profit (€)')
-    # ax[1].grid()
-    # ax[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))  
-    # plt.tight_layout()
+    # 5. Plot Profit per Scenario vs Beta
+    fig, ax = plt.subplots(1,2,figsize=(12, 3))
+    colors = plt.get_cmap('tab10').colors  # 10 colores bien diferenciados
+    color_map = {0: colors[0], 0.8: colors[1], 1: colors[2]}
+    j = -1
+    for i, res in enumerate(results):
+        if res['beta'] == 0 or res['beta'] == 1 or res['beta'] == 0.5:
+            j += 1
+            ax[0].hist(res['profit_per_scenario'], label=f"$\\beta = {res['beta']:.2f}$",
+                    bins=30, alpha=0.3, color=colors[j], edgecolor='black')
+    ax[0].legend()
+    ax[0].grid()
+    ax[0].set_xlabel('Profit per Scenario (€)')
+    ax[0].set_ylabel('Frequency')
+    ax[1].plot(beta_values, [res['total_expected_profit'] for res in results], '-')
+    ax[1].set_xlabel('Risk Weight (Beta)')
+    ax[1].set_ylabel('Total Expected Profit (€)')
+    ax[1].grid()
+    ax[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))  
+    plt.tight_layout()
 
 
-    # # Plot bids for different insample sizes with beta = 0.5
+    # Plot bids for different insample sizes with beta = 0.5
     
-    # beta = 0.5
-    # sample_sizes = [100, 200, 300, 400, 500]
-    # bids_dict = {}
+    beta = 0.5
+    sample_sizes = [100, 200, 300, 400, 500]
+    bids_dict = {}
 
-    # for n in sample_sizes:
-    #     sampled_combinations = random.sample(all_combinations, n)
-    #     scenarios = {i+1: {
-    #         'rp': rp_scenarios[rp_index],
-    #         'sc': sc_scenarios[sc_index],
-    #         'eprice': eprice_scenarios[eprice_index]
-    #     } for i, (rp_index, sc_index, eprice_index) in enumerate(sampled_combinations)}
+    for n in sample_sizes:
+        sampled_combinations = random.sample(all_combinations, n)
+        scenarios = {i+1: {
+            'rp': rp_scenarios[rp_index],
+            'sc': sc_scenarios[sc_index],
+            'eprice': eprice_scenarios[eprice_index]
+        } for i, (rp_index, sc_index, eprice_index) in enumerate(sampled_combinations)}
         
-    #     input_data = InputData(T, list(range(1, n+1)), scenarios, prob_scenario, model_type=model_type)
-    #     model = RiskAverseExPostAnalysis(
-    #         input_data=input_data,
-    #         beta=beta,
-    #         alpha=0.90,   # CVaR confidence level (as per task)
-    #         verbose=False # Disable detailed logging for batch runs
-    #     )
-    #     model.run() 
-    #     bids = [model.results.production[t] for t in model.data.T]       
-    #     bids_dict[n] = bids
+        input_data = InputData(T, list(range(1, n+1)), scenarios, prob_scenario, model_type=model_type)
+        model = RiskAverseExPostAnalysis(
+            input_data=input_data,
+            beta=beta,
+            alpha=0.90,   # CVaR confidence level (as per task)
+            verbose=False # Disable detailed logging for batch runs
+        )
+        model.run() 
+        bids = [model.results.production[t] for t in model.data.T]       
+        bids_dict[n] = bids
 
-    # plt.figure(figsize=(10,6))
-    # colors = plt.get_cmap('tab10').colors 
-    # markers = ['o', 's', 'D', '^', 'v']
-    # for idx, (n, bids) in enumerate(bids_dict.items()):
-    #     plt.plot(
-    #         T, bids,
-    #         label=f'In-sample size {n}',
-    #         color=colors[idx % len(colors)],
-    #         linewidth=2.5,
-    #         marker=markers[idx % len(markers)],
-    #         markersize=6,
-    #         alpha=0.85
-    #     )
-    # plt.xlabel('Hour')
-    # plt.ylabel('Bid (MW)')
-    # plt.title(f'Optimal Bids for Different In-sample Sizes (beta={beta})')
-    # plt.legend()
-    # plt.grid(True)
+    plt.figure(figsize=(10,6))
+    colors = plt.get_cmap('tab10').colors 
+    markers = ['o', 's', 'D', '^', 'v']
+    for idx, (n, bids) in enumerate(bids_dict.items()):
+        plt.plot(
+            T, bids,
+            label=f'In-sample size {n}',
+            color=colors[idx % len(colors)],
+            linewidth=2.5,
+            marker=markers[idx % len(markers)],
+            markersize=6,
+            alpha=0.85
+        )
+    plt.xlabel('Hour')
+    plt.ylabel('Bid (MW)')
+    plt.title(f'Optimal Bids for Different In-sample Sizes (beta={beta})')
+    plt.legend()
+    plt.grid(True)
     plt.show()
